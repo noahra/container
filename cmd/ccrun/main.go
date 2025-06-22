4d4e80096076274b253969f9cdb48e82b14f0f18
noahra <noah.rahimzadagan@protonmail.com>
2025-06-22 14:25:43 +0200

    moved out cgroups

--- cmd/ccrun/main.go
+++ cmd/ccrun/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -45,54 +43,5 @@ func main() {
 		fmt.Printf("DEBUG: Cleaning up cgroups...\n")
 
 		cleanupCgroups(HOSTNAME)
-	}
-}
-func cg(pid int, hostname string) {
-	// CPU Controller
-	cpuCgroupPath := filepath.Join("/sys/fs/cgroup/cpu", hostname)
-	err := os.MkdirAll(cpuCgroupPath, 0755)
-	if err != nil {
-		fmt.Printf("Error creating CPU cgroup: %s\n", err)
-		return
-	}
-
-	// Set CPU limits
-	cpuQuotaFile := filepath.Join(cpuCgroupPath, "cpu.cfs_quota_us")
-	err = os.WriteFile(cpuQuotaFile, []byte("50000"), 0644)
-	if err != nil {
-		fmt.Printf("Error setting CPU quota: %s\n", err)
-		return
-	}
-
-	cpuPeriodFile := filepath.Join(cpuCgroupPath, "cpu.cfs_period_us")
-	err = os.WriteFile(cpuPeriodFile, []byte("100000"), 0644)
-	if err != nil {
-		fmt.Printf("Error setting CPU period: %s\n", err)
-		return
-	}
-	// Set PID limits
-	pidsMaxFile := filepath.Join(cpuCgroupPath, "pids.max")
-	err = os.WriteFile(pidsMaxFile, []byte("20"), 0644)
-	if err != nil {
-		fmt.Printf("Error setting PID limit: %s\n", err)
-		return
-	}
-
-	// Add PID to CPU cgroup
-	cpuProcsFile := filepath.Join(cpuCgroupPath, "cgroup.procs")
-	err = os.WriteFile(cpuProcsFile, []byte(fmt.Sprintf("%d", pid)), 0644)
-	if err != nil {
-		fmt.Printf("Error adding PID to CPU cgroup: %s\n", err)
-		return
-	}
-
-}
-
-func cleanupCgroups(hostname string) {
-	// Clean up CPU cgroup
-	cpuCgroupPath := filepath.Join("/sys/fs/cgroup/cpu", hostname)
-	err := os.RemoveAll(cpuCgroupPath)
-	if err != nil {
-		fmt.Printf("Error removing CPU cgroup: %s\n", err)
 	}
 }
