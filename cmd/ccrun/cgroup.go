eda45e43da77a71a30d60c9b38fcb426abdec52a
noahra <noah.rahimzadagan@protonmail.com>
2025-06-28 11:37:37 +0200

    cgroup support added

--- cmd/ccrun/cgroup.go
+++ cmd/ccrun/cgroup.go
@@ -8,40 +8,19 @@ import (
 
 func cg(pid int, hostname string) {
 
-	subtreeControlTreePath := "/sys/fs/cgroup"
+	rootCgroupPath := "/sys/fs/cgroup"
+	hostnameCgroupPath := filepath.Join(rootCgroupPath, hostname)
 
-	subtreeControlFile := filepath.Join(subtreeControlTreePath, "cgroup.subtree_control")
-
-	current, err := os.ReadFile(subtreeControlFile)
-	if err != nil {
-		fmt.Printf("Error reading current state: %s\n", err)
-		return
-	}
-	// Parse and add new controllers (more complex logic needed here)
-	newContent := string(current) + " +pids"
-	err = os.WriteFile(subtreeControlFile, []byte(newContent), 0644)
-	if err != nil {
-		fmt.Printf("Error enabling controllers: %s\n", err)
-		return
-	}
-
-	hostnameCgroupPath := filepath.Join("/sys/fs/cgroup/", hostname)
-	err = os.MkdirAll(hostnameCgroupPath, 0755)
+	err := os.MkdirAll(hostnameCgroupPath, 0755)
 	if err != nil {
 		fmt.Printf("Error creating container's cgroup: %s\n", err)
 		return
 	}
+
 	cpuProcsFile := filepath.Join(hostnameCgroupPath, "cgroup.procs")
 	err = os.WriteFile(cpuProcsFile, []byte(fmt.Sprintf("%d", pid)), 0644)
 	if err != nil {
 		fmt.Printf("Error adding PID to CPU cgroup: %s\n", err)
-		return
-	}
-
-	pidsMaxFile := filepath.Join(hostnameCgroupPath, "pids.max")
-	err = os.WriteFile(pidsMaxFile, []byte("20"), 0644)
-	if err != nil {
-		fmt.Printf("Error setting PIDs limit: %s\n", err)
 		return
 	}
 
